38e092680fbe460cc28837c1f273915a1ddea605
Dement057 <Dement057@bitbucket.org>
2020-05-08 23:53:23 +0300

    AddMovieInList

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,1 +1,56 @@
-const firstC= {name: 'Vova'};
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchMobieBtn = document.getElementById("search-btn");
+const movieList = document.getElementById('movie-list');
+const movies = [];
+
+const toggleMovieItem = () => {
+    movieList.style = 'visible';
+}
+
+const renderMovies = () => {
+    if (movies.length === 0) {
+        movieList.classList.remove('visible');
+    } else {
+        movieList.classList.add('visible');
+    }
+movieList.innerHTML = '';
+
+movies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+})
+}
+
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random()
+  };
+
+  movies.push(newMovie);
+  console.log(newMovie);
+  toggleMovieItem()
+  renderMovies()
+};
+
+
+
+const New = () => {console.log('click')};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
